67213c07a018428e56a5d2573b4c29c981b1eb5f
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-05-18 12:30:51 +0530

    adding struct for file_system

---
+++ file_system/structs.go
@@ -0,0 +1,75 @@
+package filesystem
+
+import "time"
+
+type User struct {
+	ID     string
+	Name   string
+	Email  string
+	Files  *[]File
+	Shared *[]SharedFile
+}
+
+type File struct {
+	Id          string
+	Name        string
+	OwnerId     string
+	Size        int64
+	Path        string
+	Permissions *[]Permission
+	Versions    *[]Version
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	IsDirectory bool
+}
+
+type SharedFile struct {
+	FileID     string
+	SharedBy   string
+	SharedWith string
+	AccessType AccessType
+	SharedAt   time.Time
+}
+
+type Permission struct {
+	UserID     string
+	FileID     string
+	AccessType AccessType
+}
+
+type Version struct {
+	VersionID string
+	FileID    string
+	CreatedAt time.Time
+	Path      string
+	Size      int64
+}
+
+type AccessType int
+
+const (
+	Read  AccessType = iota
+	Write AccessType
+	Owner AccessType
+)
+
+/*
+
+
+File system
+
+
+root folder.
+
+all other folders.
+Each folder has owners. And shared with whom.
+
+
+Root folder should list all the owner folders.
+Shared folder should have list of all things shared with that person.
+
+What other entity you think we should have?
+
+
+
+*/
