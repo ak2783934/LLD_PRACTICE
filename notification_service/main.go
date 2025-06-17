aebc6466b1076a8f86e378c4118e7c0e931c5d25
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-06-17 13:19:39 +0530

    adding notification_service

---
+++ notification_service/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type Notification struct {
+	// Add fields here if needed
+}
+
+func (n *Notification) Send(to, message string) error {
+	// Implement the send logic here
+	return nil
+}
+
+type EmailSender struct{}
+
+func (e *EmailSender) Send(to, message string) error {
+	fmt.Printf("Email sent to %s: %s\n", to, message)
+	return nil
+}
+
+type SMSSender struct{}
+
+func (s *SMSSender) Send(to, message string) error {
+	fmt.Printf("SMS sent to %s: %s\n", to, message)
+	return nil
+}
+
+type PushSender struct{}
+
+func (p *PushSender) Send(to, message string) error {
+	fmt.Printf("Push sent to %s: %s\n", to, message)
+	return nil
+}
+
+type NotificationService struct {
+	senders map[string]Notification
+}
+
+func NewNotificationService() *NotificationService {
+	return &NotificationService{
+		senders: map[string]Notification{
+			"email": EmailSender{},
+			"push":  PushSender{},
+			"sms":   SMSSender{},
+		},
+	}
+}
+
+func (ns *NotificationService) Send(channel, to, message string) {
+	sender, ok := ns.senders[channel]
+	if !ok {
+		return fmt.Errorf("unsupported channel: %s", channel)
+	}
+	return sender.Send(to, message)
+}
